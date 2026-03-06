33b3453de1b227062549ad9397227b382c2cf679
dabel-sys <d.abel@skylva.com>
2026-03-06 18:20:27 +0100

    14

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import { useEffect } from 'react';
+import Lenis from 'lenis';
+
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import ValuePillars from './components/ValuePillars';
@@ -13,6 +16,24 @@ import ForProfessionals from './components/ForProfessionals';
 import Footer from './components/Footer';
 
 export default function App() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    });
+
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
   return (
     <div className="bg-obsidian text-white min-h-screen selection:bg-champagne selection:text-white">
       <Navbar />
